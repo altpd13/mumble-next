d969e4ee5bf5cc0fa9e6b99ee63abb4915f5360d
minseokkim <altpd13@gmail.com>
2020-11-28 15:12:00 +0900

    Added Form for add port and stuff plus added Error handling for client and server

--- pages/test3/index.tsx
+++ pages/test3/index.tsx
@@ -1,42 +1,108 @@
-import React,{Component} from 'react'
+import React, {Component} from 'react'
 
 // @ts-ignore
 import ExampleWorker from "../../workers/ex.worker";
+import TestWorker from "../../workers/test.worker"
 import Promise from "promise";
 
-export default class Test3 extends Component<any,any> {
-  worker: any;
-  constructor(props:any) {
+export default class Test3 extends Component<any, any> {
+  worker: Worker;
+  testWorker: Worker;
+  client: any;
+
+  constructor(props: any) {
     super(props)
     this.state = {
-      isConnected: false
+      isConnected: false,
+      address: '',
+      port: '',
+      username: '',
+      password: ''
+
     }
     this.connectMumble = this.connectMumble.bind(this);
+    this.handleChange = this.handleChange.bind(this)
   }
 
-  connectMumble() {
-    this.setState((state:any) => ({
+  handleChange = (event: any) => {
+
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+
+  connectMumble(event:any) {
+    event.preventDefault()
+    this.setState((state: any) => ({
       isConnected: !state.isConnected
     }))
     this.worker.postMessage('from Host')
-    const mc = new mumbleConnector(this.worker)
-    mc.connect(`ws://127.0.0.1:8889`,{
-      username: 'a',
-      password: 'a',
-      tokens: 'a'
-    })
+    const mc = new mumbleConnector(this.worker) //MC in da house
+    mc.connect(`ws://${this.state.address}:${this.state.port}`, {
+      username: this.state.username,
+      password: this.state.username,
+      tokens: ''
+    }).done(client => {
+        console.log('I guess...Connected?')
+        this.client = client
+
+        client.on('error', (err: any) => {
+          console.log(`Error: ${err}`)
+        })
+      }, err => {
+        if (err.$type && err.$type.name === 'Reject') {
+        console.log(err.type)
+          console.log(err.reason)
+        }else console.log(`Well You got error while initializing Client LOL`)
+      }
+    )
   }
 
   componentDidMount() {
     this.worker = new ExampleWorker()
+    this.testWorker = new TestWorker()
+    this.testWorker.postMessage('Hello to Worker')
   }
+
+  logWorkerMessage = (event: { data: any }) => console.log(event.data)
 
   render() {
     return (
       <div>
-        <button onClick={this.connectMumble}>
-          {this.state.isConnected? 'Connected to Server' : 'Connect to Server'}
-        </button>
+        <form onSubmit={this.connectMumble}>
+          <div>
+            <label>
+              Address:
+              <input name={'address'} type="text" value={this.state.address} onChange={this.handleChange} required={true}/>
+            </label>
+          </div>
+
+          <div>
+            <label>
+              Port:
+              <input name={'port'} type="text" value={this.state.port} onChange={this.handleChange} required={true}/>
+            </label>
+          </div>
+
+          <div>
+            <label>
+              User Name:
+              <input name={'username'} type="text" value={this.state.username} onChange={this.handleChange} required={true}/>
+            </label>
+          </div>
+
+          <div>
+            <label>
+              Password:
+              <input name={'password'} value={this.state.password} onChange={this.handleChange} type="text"/>
+            </label>
+          </div>
+
+          <div>
+            <input type="submit" value={this.state.isConnected ? 'Connected to Server' : 'Connect to Server'}></input>
+          </div>
+        </form>
       </div>
     )
   }
@@ -46,21 +112,25 @@ class mumbleConnector {
   _worker: any
   _requests: any
   _reqId: number
-  constructor(worker:any) {
+
+  constructor(worker: any) {
     this._worker = worker
     this._requests = {}
     this._reqId = 1
   }
-  connect(host:any,args:any) {
-    return this._query({},'_connect',{host:host, args: args},null)
+
+  connect(host: any, args: any) {
+    return this._query({}, '_connect', {host: host, args: args}, null)
   }
-  _query(id:any, method:any, payload:any,transfer:any|null) {
-    let reqId = this._call(id, method, payload,transfer)
+
+  _query(id: any, method: any, payload: any, transfer: any | null) {
+    let reqId = this._call(id, method, payload, transfer)
     return new Promise((resolve, reject) => {
       this._requests[reqId] = [resolve, reject]
     })
   }
-  _call(id:any, method:any, payload:any,transfer:any|null) {
+
+  _call(id: any, method: any, payload: any, transfer: any | null) {
     let reqId = this._reqId++
     console.debug(method, id, payload)
     this._postMessage({
@@ -73,7 +143,8 @@ class mumbleConnector {
     }, transfer)
     return reqId
   }
-  _postMessage(msg:any, transfer:any|null) {
+
+  _postMessage(msg: any, transfer: any | null) {
     try {
       this._worker.postMessage(msg, transfer)
     } catch (err) {
