55785b5cb97f75afe1959e5ac0225fb327a26207
minseokkim <altpd13@gmail.com>
2020-12-31 18:20:45 +0900

    Added user icon and users

--- component/ChannelContainer/ChannelContainer.tsx
+++ component/ChannelContainer/ChannelContainer.tsx
@@ -1,15 +1,12 @@
-import React from 'react'
-import Channel from './Channel'
-import User from './User'
+import React, {useEffect} from 'react'
+import {User} from './User'
+import _newUser from "../../utils/newUser";
 
-const ChannelContainer = ({ channels,setChannels,users,setUsers }:any) => {
+const ChannelContainer = ({channels, setChannels, users, setUsers}: any) => {
   return (
     <div className="channel-root-container">
-      {/*<div className="channel-root">*/}
-      {/*  {channels.map((channel:any,i:number) =><div key ={i} ><Channel className='channel' channel={channel} ></Channel></div>)}*/}
-      {/*</div>*/}
       <div className={'channel-root'}>
-        {users.map((user:any,i:number)=> <div className={'user'} key={i}><User user={user}></User></div>)}
+        {users.map((user: any, i: number) => <div key={i}><User user={user}></User></div>)}
       </div>
     </div>
   )
